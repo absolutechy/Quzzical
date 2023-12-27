c4b9859be4e8dd80335737aeefd574526cf26f62
sampad201199 <sampadsingha8@gmail.com>
2023-12-27 16:47:38 +0600

    Solved issue of map undefined

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -5,27 +5,33 @@ function QuizPage() {
 
     const [quiz, setQuiz] = useState([])
 
-    useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-        .then(res => res.json())
-        .then(data => setQuiz(data.results))
-    }, [])
 
-    console.log(quiz)
+    async function getQuiz() {
+        const response = await fetch("https://opentdb.com/api.php?amount=5&category=18&type=multiple");
+        const data = await response.json();
+        setQuiz(data.results)
+    }
+    function handleClick() {
+        getQuiz().then(r => {
+            console.log(r)
+        })
+    }
 
-    const quizArray =  quiz.map(item => {
-        return <Questions  
-        question={item.question} 
-        options={[item.correct_answer, ...item.incorrect_answers]} 
+    const quizArray = quiz.map(item => {
+        const options=[...item.incorrect_answers]
+        options.push(item.correct_answer)
+        return <Questions
+            key={item.question}
+            question={item.question}
+            options={options}
         />
     })
-  return (
-    <div className="quiz-container">
-        <form>
+    return (
+        <div className="quiz-container">
+            <button onClick={handleClick}>Click me</button>
             {quizArray}
-        </form>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default QuizPage
