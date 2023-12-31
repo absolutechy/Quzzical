c35c98990e957e04a210fc0d97bcb74658ab7228
Sheeraz Ahmed <ahmedservices101@gmail.com>
2023-12-31 19:02:20 +0500

    some bugs are occuring and 429 error

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
+// App Component
 import {useState, useEffect, useRef} from "react"
 import Questions from "./components/Questions.jsx"
 import StartingPage from './components/StartingPage.jsx'
-// import QuizPage from './components/QuizPage.jsx'
 import './App.css'
 
-
 function App() {
-
     const [quiz, setQuiz] = useState(null)
     const firstload = useRef(true)
 
-    function startQuiz() {
-      useEffect(() => {
-          if(firstload.current) {
-              fetch("https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple")
-                  .then(res => res.json())
-                  .then(data => setQuiz(data.results))
-              firstload.current = false
-          }
-  
-      }, [])
+    useEffect(() => {
+        if(firstload.current) {
+            fetch("https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple")
+                .then(res => res.json())
+                .then(data => setQuiz(data.results))
+            firstload.current = false
+        }
+    }, [firstload])
+
+    const startQuiz = () => {
+        firstload.current = true
     }
-
-
 
     const quizArray = quiz && quiz.map(item => {
         const optionsArray = [...item.incorrect_answers, item.correct_answer]
@@ -34,9 +31,10 @@ function App() {
     })
     return (
         <div className="quiz-container">
-            {quiz ? <StartingPage/> : <form>{quizArray}</form>}
+            {quiz ? <form>{quizArray}</form> : <StartingPage startQuiz = {startQuiz}/>}
         </div>
     )
 }
 
 export default App
+
