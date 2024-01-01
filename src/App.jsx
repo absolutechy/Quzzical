45f8f4fb2e5d4cc3805263be3b03f8df56f83bae
Sheeraz Ahmed <ahmedservices101@gmail.com>
2024-01-01 10:21:04 +0500

    Fully Working Code But Quiz Checking not implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-// App Component
-import {useState, useEffect, useRef} from "react"
+import {useState, useEffect} from "react"
 import Questions from "./components/Questions.jsx"
 import StartingPage from './components/StartingPage.jsx'
 import './App.css'
 
 function App() {
     const [quiz, setQuiz] = useState(null)
-    const firstload = useRef(true)
+    const [start, setStart] = useState(false) // New state variable
 
     useEffect(() => {
-        if(firstload.current) {
+        if(start) {
             fetch("https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple")
                 .then(res => res.json())
                 .then(data => setQuiz(data.results))
-            firstload.current = false
+            setStart(false) // Reset the start state after fetching the data
         }
-    }, [firstload])
+    }, [start]) // Add start to the dependency array
 
     const startQuiz = () => {
-        firstload.current = true
+        setStart(true) // Change the start state to true when the button is clicked
     }
 
     const quizArray = quiz && quiz.map(item => {
@@ -37,4 +36,3 @@ function App() {
 }
 
 export default App
-
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,22 +1,37 @@
 import {useId} from "react"
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 function Questions(props) {
     const id = useId()
-    const answersArray = props.options
-  return (
+    const answersArray = shuffleArray([...props.options])
+
+    function decodeHTMLEntities(text) {
+      const textArea = document.createElement('textarea');
+      textArea.innerHTML = text;
+      return textArea.value;
+    }
+
+    return (
         <div id="question1">
-            <p className="question">{props.question}</p>
-            <input type="radio" name="q1" className="option-input radio" id={`${id} + q1a`} />
-            <label htmlFor={`${id} + q1a`} className="option">{answersArray[Math.floor(Math.random() * 4 + 1)]}</label>
-            <input type="radio" name="q1" className="option-input radio" id={`${id} + q1b`} />
-            <label htmlFor={`${id} + q1b`} className="option">{answersArray[Math.floor(Math.random() * 4 + 1)]}</label>
-            <input type="radio" name="q1" className="option-input radio" id={`${id} + q1c`} />
-            <label htmlFor={`${id} + q1c`} className="option">{answersArray[Math.floor(Math.random() * 4 + 1)]}</label>
-            <input type="radio" name="q1" className="option-input radio" id={`${id} + q1d`} />
-            <label htmlFor={`${id} + q1d`} className="option">{answersArray[Math.floor(Math.random() * 4 + 1)]}</label>
+            <p className="question">{decodeHTMLEntities(props.question)}</p>
+            <input type="radio" name={`${id} + q`} className="option-input radio" id={`${id} + q1a`} />
+            <label htmlFor={`${id} + q1a`} className="option">{decodeHTMLEntities(answersArray[0])}</label>
+            <input type="radio" name={`${id} + q`} className="option-input radio" id={`${id} + q1b`} />
+            <label htmlFor={`${id} + q1b`} className="option">{decodeHTMLEntities(answersArray[1])}</label>
+            <input type="radio" name={`${id} + q`} className="option-input radio" id={`${id} + q1c`} />
+            <label htmlFor={`${id} + q1c`} className="option">{decodeHTMLEntities(answersArray[2])}</label>
+            <input type="radio" name={`${id} + q`} className="option-input radio" id={`${id} + q1d`} />
+            <label htmlFor={`${id} + q1d`} className="option">{decodeHTMLEntities(answersArray[3])}</label>
             <div className="line"></div>
         </div>
-  )
+    )
 }
 
 export default Questions
